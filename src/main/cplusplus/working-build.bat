fb77367b81b760806a932b08a42273984dd972db
Tsunko <cirnothegenius@aol.com>
2015-04-25 23:17:39 -0700

    Forgot to upload the working build.bat for Windows users.

---
+++ src/main/cplusplus/working-build.bat
@@ -0,0 +1,3 @@
+# this shouldn't be allowed, but this is the only working command line for some reason.
+# open nuwen's open_distro_window.bat and run the below.
+g++ -o NativeVideo.dll ga_nurupeaches_imgmap_natives_NativeVideo.cpp -I"C:/Program Files/Java/jdk1.8.0_31/include" -I"C:/Program Files/Java/jdk1.8.0_31/include/win32" -I"C:/FFmpeg/include" -L"C:/FFmpeg/lib" -lavutil -lavformat -lavcodec -lswscale -std=c++11 -shared'
